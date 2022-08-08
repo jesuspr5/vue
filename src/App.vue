f91332bfe8cf2508ae4220be4447adb7afbc80a5
jesuspr5 <pereirajesus35@gmail.com>
2022-08-08 23:14:33 +0200

    pratica de vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+       <HelloWorld :msg="count"/>
+      
+       <button  @click="incremenCont"   >Incrementar +1</button>
+       <button @click="decrementCont"> Decrementar -1</button>
+        <Buttton :name="nombre" :contador="count" />
+  </div> 
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Buttton from './components/Buttton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Buttton
+}
+,
+  data: ()=>({
+    count : 10,
+    nombre:"valor:",
+   
+
+  }),
+  methods:{
+    incremenCont(){
+      this.count++
+    },
+    decrementCont(){
+      this.count--
+    },
+     
   }
+  
+
+
 }
 </script>
 
@@ -24,5 +49,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button{
+ box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);;   
+background-color: #4CAF50; /* Green */
+    color: white;
+    border-radius:  4px;
+    font-size: 20px;
+
+    margin: 10px;
 }
 </style>
---
+++ src/components/Buttton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+    
+    <button>{{name}}{{contador}}  </button>
+   
+  </div>
+</template>
+
+<script>
+
+
+
+
+export default {
+  name: 'Buttton',
+  props: {
+    
+    name: String,
+    contador: Number
+    
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
